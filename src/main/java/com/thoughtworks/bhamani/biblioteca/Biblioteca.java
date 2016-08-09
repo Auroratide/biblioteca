b7c0c61ea09c3a16a2d0ad03069cb26fcbe8ba1a
shaktech786 <shakeel.bhamani@gmail.com>
2016-08-09 15:37:15 -0500

    Second User Story completed

--- src/main/java/com/thoughtworks/bhamani/biblioteca/Biblioteca.java
+++ src/main/java/com/thoughtworks/bhamani/biblioteca/Biblioteca.java
@@ -1,15 +1,23 @@
 package com.thoughtworks.bhamani.biblioteca;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class Biblioteca {
     private PrintStream printStream;
+    private ArrayList<String> library;
 
-    public Biblioteca(PrintStream printStream) {
+    public Biblioteca(PrintStream printStream, ArrayList<String> initialLibrary) {
         this.printStream = printStream;
+        this.library = initialLibrary;
     }
 
     public void start() {
         printStream.println("Welcome!");
+    }
+
+    public void printLibraryList() {
+        for(String book : library)
+            printStream.println(book);
     }
 }
--- src/test/java/com/thoughtworks/bhamani/biblioteca/BibliotecaTest.java
+++ src/test/java/com/thoughtworks/bhamani/biblioteca/BibliotecaTest.java
@@ -1,8 +1,10 @@
 package com.thoughtworks.bhamani.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -10,11 +12,37 @@ import static org.mockito.Mockito.verify;
 
 public class BibliotecaTest {
 
+    private PrintStream printStream;
+    private Biblioteca biblioteca;
+
+    @Before
+    public void setUp() throws Exception {
+        printStream = mock(PrintStream.class);
+        biblioteca = new Biblioteca(printStream, makePopulatedLibrary());
+
+    }
+
     @Test
     public void shouldSeeWelcomeMessageWhenStarted() {
-        PrintStream printStream = mock(PrintStream.class);
-        Biblioteca biblioteca = new Biblioteca(printStream);
         biblioteca.start();
         verify(printStream).println("Welcome!");
+    }
+
+    @Test
+    public void shouldSeeListOfBooksInLibraryAfterWelcome() {
+        biblioteca.printLibraryList();
+        verify(printStream).println("Book 3");
+    }
+
+    private ArrayList<String> makeEmptyLibrary() {
+        return new ArrayList<String>();
+    }
+
+    private ArrayList<String> makePopulatedLibrary() {
+        ArrayList<String> library = makeEmptyLibrary();
+        library.add("Book 1");
+        library.add("Book 2");
+        library.add("Book 3");
+        return library;
     }
 }
